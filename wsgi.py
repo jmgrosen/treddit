7ccf4087603ec64679eb3adfe98e51ee3c2a1e5b
John Grosen <jmgrosen@gmail.com>
2012-07-21 22:29:25 -0700

    Added treddit to INSTALLED_APPS

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'treddit_python')))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'treddit_python.settings'
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
