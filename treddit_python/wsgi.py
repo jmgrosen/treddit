51ad70d9706ed8c13238208a4df3ace86d61002a
John Grosen <jmgrosen@gmail.com>
2012-07-21 22:15:02 -0700

    Added more stuff for mongo

--- treddit_python/wsgi.py
+++ treddit_python/wsgi.py
@@ -1,6 +1,8 @@
 import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'treddit_python')))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'treddit_python.settings'
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "treddit_python.settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
